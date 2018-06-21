09c4083cfe1712dabd1a24ca70acf76e299beba3
Andreas Moor <amoor@fastmail.net>
2018-06-21 09:04:16 +0200

    Add swipeable

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,154 +1,224 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { css } from 'glamor'
+import zIndex from '../../theme/zIndex'
+
+import Swipeable from 'react-swipeable'
 import Close from 'react-icons/lib/md/close'
 import ChevronLeft from 'react-icons/lib/md/chevron-left'
 import ChevronRight from 'react-icons/lib/md/chevron-right'
 
-const styles = {
-    //Gallery
-    gallery: css({
-        color: 'white',
-        background: 'black',
-        height: '100vh'
-    }),
-    //Header
-    header: css({
-        display: 'flex',
-        justifyContent: 'space-between',
-        position: 'relative',
-        height: '30px',
-        padding: '10px',
-    }),
-    counter: css({
-        flex: 1,
-    }),
-    close: css({
-        position: 'absolute',
-        flex: 1,
-        right: 0,
-        top: 0,
-    }),
-    //Item
-    mediaItem: css({
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-        position: 'relative',
-        flex: 1,
-        flexGrow: 1,
-        '& > *': {
-            flex: '0 0 auto',
-            verticalAlign: 'bottom',
-            position: 'absolute', // make the image absolute
-            left: 0, // and position it in the center
-            right: 0,
-            top: 0,
-            bottom: 0,
-            maxWidth: '100%',
-            maxHeight: '100%',
-            margin: 'auto',
-        }
+const mediaItemFadeIn = css.keyframes({
+  '0%': {opacity: 0},
+  '100%': {opacity: 1}
+})
 
-    }),
-    item: css({
-        position: 'absolute',
-        top: '30px',
-        left: 0,
-        bottom: 0,
-        right: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        height: 'calc(100% - 30px)',
-    }),
-    buttonLeft: css({
-        position: 'absolute',
-        top: '45%',
-        left: 0,
-    }),
-    buttonRight: css({
-        position: 'absolute',
-        top: '45%',
-        right: 0,
-    }),
-    media: css({
-        height: '100px',
-    }),
-    caption: css({
-        padding: '10px',
-    })
+
+const styles = {
+  gallery: css({
+    color: '#fff',
+    background: '#000',
+    width: '100%',
+    height: '100vh'
+  }),
+  header: css({
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'relative',
+    height: '30px',
+    padding: '10px',
+  }),
+  counter: css({
+    flex: 1,
+  }),
+  close: css({
+    position: 'absolute',
+    flex: 1,
+    right: 0,
+    top: 0,
+  }),
+  mediaItem: css({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    position: 'relative',
+    flex: 1,
+    flexGrow: 1,
+    opacity: 1,
+    animation: `${mediaItemFadeIn} 0.2s linear`,
+    '& > *': {
+      flex: '0 0 auto',
+      verticalAlign: 'bottom',
+      position: 'absolute', // make the image absolute
+      left: 0, // and position it in the center
+      right: 0,
+      top: 0,
+      bottom: 0,
+      maxWidth: '100%',
+      maxHeight: '100%',
+      margin: 'auto',
+    }
+  }),
+  item: css({
+    position: 'absolute',
+    top: '30px',
+    left: 0,
+    bottom: 0,
+    right: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    height: 'calc(100% - 30px)',
+  }),
+  nav: css({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100vw',
+    height: '100vh',
+    color: '#fff',
+    zIndex: zIndex.overlay,
+    '& .left': {
+      justifyContent: 'flex-start',
+    },
+    '& .right': {
+      justifyContent: 'flex-end'
+    }
+  }),
+  navArea: css({
+    display: 'flex',
+    alignItems: 'center',
+    width: '50%',
+    height: '100vh',
+    padding: '0 50px',
+    opacity: 0,
+    transition: 'opacity 0.5s linear',
+    ':hover': {
+      transition: 'opacity 0.5s linear',
+      opacity: '0.7'
+    }
+  }),
+  navButton: css({
+    color: '#fff',
+  }),
+  media: css({
+    height: '100px',
+  }),
+  caption: css({
+    padding: '10px',
+  })
 }
 
 class Gallery extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            index: 0,
-        }
-        this.handleClickLeft = this.handleClickLeft.bind(this)
-        this.handleClickRight = this.handleClickRight.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: +props.startItem || 0,
+      swipePos: 0
     }
+    this.handleClickLeft = this.handleClickLeft.bind(this)
+    this.handleClickRight = this.handleClickRight.bind(this)
+  }
 
-    handleClickLeft() {
-        const total = this.props.items.length
-        this.setState(prevState => ({
-            index: prevState.index !== 0 ? (prevState.index - 1) : (total - 1),
-        }))
-    }
+  handleClickLeft() {
+    const total = this.props.items.length
+    this.setState(prevState => ({
+      index: prevState.index !== 0 ? (prevState.index - 1) : (total - 1),
+    }))
+  }
 
-    handleClickRight() {
-        const total = this.props.items.length - 1
-        this.setState(prevState => ({
-            index: prevState.index !== total ? (prevState.index + 1) : 0,
-        }))
-    }
+  handleClickRight() {
+    const total = this.props.items.length - 1
+    this.setState(prevState => ({
+      index: prevState.index !== total ? (prevState.index + 1) : 0,
+    }))
+  }
 
-    render() {
-        const { index } = this.state
-        const { mediaItem, captionItem, aspectRatio } = this.props.items[index]
-        const total = this.props.items.length
-        return (
-            <div {...styles.gallery}>
-                <Header index={index + 1} total={total} />
-                <Item mediaItem={mediaItem} aspectRatio={aspectRatio} captionItem={captionItem} handleClickLeft={this.handleClickLeft} handleClickRight={this.handleClickRight} />
+  render() {
+    const { index } = this.state
+    const { onClose } = this.props
+    const { mediaItem, captionItem, aspectRatio } = this.props.items[index]
+    const total = this.props.items.length
+    return (
+      <Swipeable 
+        // onSwipedLeft={this.handleClickLeft} 
+        // onSwipedRight={this.handleClickRight} 
+        onSwipedDown={onClose ? onClose : () => {}}
+        onTap={() => {}}
+        delta={10}
+        preventDefaultTouchmoveEvent={true}
+        stopPropagation={true}
+        onSwiping={(e, deltaX, deltaY, absX, absY, velocity) => this.setState({swipePos: deltaX})}
+        onSwiped={(e, deltaX, deltaY, isFlick, velocity) => isFlick && this.setState({swipePos: 0}, this.handleClickRight) || this.setState({swipePos: 0})}
+      >
+        <Nav
+          handleClickLeft={this.handleClickLeft} 
+          handleClickRight={this.handleClickRight}           
+        />
+        <div {...styles.gallery}>
+          <Header 
+            index={index + 1} 
+            total={total} 
+            onClose={onClose}
+          />
+          <div style={{ position: 'absolute', width:'100%', height: '100%', left: -this.state.swipePos}}>
+            <div {...styles.item}>
+              <div {...styles.mediaItem}>
+                {mediaItem}
+              </div>
+              <div {...styles.caption}>{captionItem}</div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </Swipeable>
+    )
+  }
 }
 
-
-
-const Header = ({ index, total }) => (
-    <div {...styles.header}>
-        <div {...styles.counter}>
-            <span>{index}/{total}</span>
-        </div>
-        <div {...styles.close}><Close size={24} /></div>
+const Header = ({ index, total, onClose }) => (
+  <div {...styles.header}>
+    <div {...styles.counter}>
+      <span>{index}/{total}</span>
     </div>
+    <div {...styles.close} onClick={onClose}><Close size={24} /></div>
+  </div>
 )
 
+const Nav = ({ handleClickLeft, handleClickRight }) => 
+  <div {...styles.nav}>
+    <div {...styles.navArea} className='left' onClick={handleClickLeft}>
+      <div {...styles.navButton}>
+        <ChevronLeft size={64} />
+      </div>
+    </div>
+    <div {...styles.navArea} className='right' onClick={handleClickRight}>
+      <div {...styles.navButton}>
+        <ChevronRight size={64} />
+      </div>
+    </div>
+  </div>
+
+
 const Item = ({
-    mediaItem,
-    captionItem,
-    aspectRatio,
-    handleClickLeft,
-    handleClickRight
+  mediaItem,
+  captionItem,
+  aspectRatio
 }) => (
-        <div {...styles.item}>
-
-            <div {...styles.mediaItem}>{mediaItem}</div>
-            <div>
-                <button {...styles.buttonLeft} onClick={handleClickLeft}><ChevronLeft size={48} /></button>
-                <button {...styles.buttonRight} onClick={handleClickRight}><ChevronRight size={48} /></button>
-            </div>
-
-            <div {...styles.caption}>{captionItem}</div>
-        </div>
-    )
+  <div {...styles.item}>
+    <div {...styles.mediaItem}>
+      {mediaItem}
+    </div>
+    <div {...styles.caption}>{captionItem}</div>
+  </div>
+)
 
 Gallery.propTypes = {
-    items: PropTypes.array.isRequired,
+  items: PropTypes.array.isRequired,
+  onClose: PropTypes.func
 }
 
 export default Gallery
---
+++ src/components/Gallery/Gallery.story.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withState } from '@dump247/storybook-state'
+
+
+import { FigureCaption, FigureByline } from "../Figure"
+import Gallery from './Gallery'
+
+const items = [
+  {
+    mediaItem: <img src='/static/landscape.jpg' />,
+    captionItem: <FigureCaption>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. <FigureByline>Photo: Laurent Burst</FigureByline></FigureCaption>,
+  },
+  {
+    mediaItem: <img src='/static/rothaus_portrait.jpg' />,
+    captionItem: <FigureCaption>One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly.<FigureByline>Photo: Laurent Burst</FigureByline></FigureCaption>,
+  },
+  {
+    mediaItem: <img src='https://assets.republik.ch/images/pierre_rom.jpeg' />,
+    captionItem: <FigureCaption>One morning, when Gregor Samsa woke from troubled dreams.<FigureByline>Photo: Laurentine Verylong Burst</FigureByline></FigureCaption>,
+  }
+]
+
+storiesOf('Gallery', module)
+  .add('default', withState({ showGallery: false, startItem: 0 })(({ store }) =>
+    <div>
+      {
+        items.map((item, i) =>
+          <div style={{ width: '50%', margin: 'auto' }}>
+            { React.cloneElement(item.mediaItem, {key: i, style: { width: '100%' }, onClick: () => store.set({ showGallery: true, startItem: i })}) }
+          </div>
+        )
+      }
+      { store.state.showGallery &&
+        <div style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0
+        }}>
+          <Gallery
+            items={items}
+            startItem={store.state.startItem}
+            onClose={() => store.set({ showGallery: false })}
+          />
+        </div>
+      }
+    </div>
+  )
+)
